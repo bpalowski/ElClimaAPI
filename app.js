95f989b340fa91d4f724a0f1885633ab6cdf4efd
b.palowski <brianpalowski00@gmail.com>
2019-02-14 19:49:46 -0800

    Func getWeather

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 // 
 const weather = new Weather('98058', 'us');
 
-weather.changeWeather('91950', 'us');
+document.addEventListener('DOMContentLoaded', getWeather);
 
+//weather.changeWeather('91950', 'us');
 
-weather.getWeather()
-	.then(function (results) {
-		console.log(results);
-	})
-	.catch(function (err) {
-		console.log(err);
-	});
+function getWeather() {
+
+	weather.getWeather()
+		.then(function (results) {
+			console.log(results);
+		})
+		.catch(function (err) {
+			console.log(err);
+		});
+}
